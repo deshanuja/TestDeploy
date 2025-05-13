260fb1ac0ec9d443b875d72229f5c5d38060d1b9
deshanuja <anujapadhye14@gmail.com>
2025-05-14 00:20:15 +0530

    Test change

--- src/main/java/org/testdeploy/testdeploy/TestDeployApplication.java
+++ src/main/java/org/testdeploy/testdeploy/TestDeployApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestDeployApplication {
-    // This is test change for jenkins with ngrok
+    // This is test change for jenkins
     public static void main(String[] args) {
         SpringApplication.run(TestDeployApplication.class, args);
     }
