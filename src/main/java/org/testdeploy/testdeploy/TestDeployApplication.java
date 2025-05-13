ddd57a1712d932600445e943b106a421ec6f73ae
deshanuja <anujapadhye14@gmail.com>
2025-05-13 23:04:01 +0530

    test

---
+++ src/main/java/org/testdeploy/testdeploy/TestDeployApplication.java
@@ -0,0 +1,13 @@
+package org.testdeploy.testdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/testdeploy/testdeploy/TestDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package org.testdeploy.testdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
