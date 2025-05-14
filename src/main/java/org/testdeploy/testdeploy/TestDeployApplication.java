310d5b0b542a15881ad5af945b3188afb989c23e
deshanuja <anujapadhye14@gmail.com>
2025-05-14 21:45:34 +0530

    Test change

--- src/main/java/org/testdeploy/testdeploy/TestDeployApplication.java
+++ src/main/java/org/testdeploy/testdeploy/TestDeployApplication.java
@@ -2,12 +2,20 @@ package org.testdeploy.testdeploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController("started")
 @SpringBootApplication
 public class TestDeployApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestDeployApplication.class, args);
         System.out.println("App Started and deployed");
+    }
+
+    @GetMapping("/get")
+    public String getData(){
+        return "App started via Jenkins";
     }
 
 }
