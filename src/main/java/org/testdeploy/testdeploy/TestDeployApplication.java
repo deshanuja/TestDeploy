28fe749076e6347e2b218c30ff154041cbd2c2d2
deshanuja <anujapadhye14@gmail.com>
2025-05-14 11:51:06 +0530

    Test change

--- src/main/java/org/testdeploy/testdeploy/TestDeployApplication.java
+++ src/main/java/org/testdeploy/testdeploy/TestDeployApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestDeployApplication {
     // This is test change for jenkins
     public static void main(String[] args) {
+
         SpringApplication.run(TestDeployApplication.class, args);
+        System.out.println("");
+
     }
 
 }
