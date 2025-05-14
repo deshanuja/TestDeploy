efea861bc5c9e3ef5e57ee40335f53ff133aefeb
deshanuja <anujapadhye14@gmail.com>
2025-05-14 21:42:50 +0530

    Test change

--- src/main/java/org/testdeploy/testdeploy/TestDeployApplication.java
+++ src/main/java/org/testdeploy/testdeploy/TestDeployApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestDeployApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestDeployApplication.class, args);
-        System.out.println("App Started");
+        System.out.println("App Started and deployed");
     }
 
 }
